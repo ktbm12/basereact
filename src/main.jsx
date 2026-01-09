b61b0d4667e78285fa2ff1e6ecc1c198bddfdd94
ktbm <boliventtalla12@gmail.com>
2026-01-09 11:08:00 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,18 @@
-import { createRoot } from 'react-dom/client'
-
-function Car(props) {
+function Car() {
   return (
-    <h2>I am a {props.color} Car!</h2>
-  );
-}
-function Cars(props) {
-  return (
-    <table>
-      <tr>
-        <Car color="red"/>
-        <Car color="blue"/>
-        <Car color="green"/>
-      </tr>
-      <tr>
-        <Car color="red"/>
-        <Car color="blue"/>
-        <Car color="green"/>
-      </tr>
-      <tr>
-        <Car color="red"/>
-        <Car color="blue"/>
-        <Car color="green"/>
-      </tr>
-    </table>
+    <h2>I am a Car!</h2>
   );
 }
 
-
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <Car color="red"/>
-    <Cars/>
-  </>
+  <Garage />
 );
