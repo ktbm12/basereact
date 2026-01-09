41ac941a5884bdea3bc20fb6c2fa2604db97a1d9
ktbm <boliventtalla12@gmail.com>
2026-01-09 12:28:32 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,17 @@ class Cars extends React.Component {
     return <h2>Hi, I am a Car!</h2>;
   }
 }
+
 createRoot(document.getElementById('root')).render(
   <>
-    <Car />
-    <Cars />
-  </>
+
+  <Cars >
+    <Cars>
+      <Car />
+      <Car />
+      <Car />
+    </Cars>
+    </Cars>
+    </>
+  
 );
