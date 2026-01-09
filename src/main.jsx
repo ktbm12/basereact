daf2565eb6fa786d9ec25d9c98e153f878af4a42
ktbm <boliventtalla12@gmail.com>
2026-01-09 15:14:30 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import Car from './Vehicle.jsx';
-
-class Cars extends React.Component {
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
+function Car(props) {
+  return (
+    <>
+      <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
+      <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
+    </>
+  );
 }
 
+const carInfo = {
+  name: "Ford",
+  model: "Mustang",
+  color: "red",
+  year: 1969
+};
+
 createRoot(document.getElementById('root')).render(
-  <>
-    <Car />
-
-    <Cars />
-    </>
-
-     
-
-
-  
+  <Car carinfo={carInfo} />
 );
+
 
                     
