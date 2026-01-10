acfd1135eddf90f235853371193a0d3fbbdf09ed
ktbm <boliventtalla12@gmail.com>
2026-01-10 23:03:55 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,73 +1,35 @@
 import { createRoot } from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-function Home() {
-  return <h1>Home Page</h1>;
-}
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
 
-function Products() {
   return (
-    <div>
-      <h1>Products Page</h1>
-      <nav style={{ marginBottom: '20px' }}>
-        <Link to="/products/car">Cars</Link> |{" "}
-        <Link to="/products/bike">Bikes</Link>
-      </nav> 
-      <Outlet /> 
-    </div>
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+      <button
+        type="button"
+        onClick={() => setColor("red")}
+      >Red</button>
+      <button
+        type="button"
+        onClick={() => setColor("pink")}
+      >Pink</button>
+      <button
+        type="button"
+        onClick={() => setColor("green")}
+      >Green</button>
+    </>
   );
 }
 
-function CarProducts() {
-  return (
-    <div>
-      <h2>Cars</h2>
-      <ul>
-        <li>Audi</li>
-        <li>BMW</li>
-        <li>Volvo</li>
-      </ul>
-    </div>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <FavoriteColor />
+);
 
-function BikeProducts() {
-  return (
-    <div>
-      <h2>Bikes</h2>
-      <ul>
-        <li>Yamaha</li>
-        <li>Suzuki</li>
-        <li>Honda</li>
-      </ul>
-    </div>
-  );
-}
-
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/products">Products</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />}>
-          <Route path="car" element={<CarProducts />} />
-          <Route path="bike" element={<BikeProducts />} />
-        </Route>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
