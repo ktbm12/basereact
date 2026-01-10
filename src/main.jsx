2f4ec9315ffccd97b0b820d94da85622ecc3227d
ktbm <boliventtalla12@gmail.com>
2026-01-10 22:38:31 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,58 @@
 import { createRoot } from 'react-dom/client'
 
-function Football() {
-  const shoot = () => {
-    alert("Great Shot!");
-  }
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function MyForm() {
+  const [selectedFruit, setSelectedFruit] = useState('banana');
+
+  const handleChange = (event) => {
+    setSelectedFruit(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    alert(`Your favorite fruit is: ${selectedFruit}`);
+    event.preventDefault();
+  };
 
   return (
-    <button onClick={shoot}>Take the shot!</button>
+    <form onSubmit={handleSubmit}>
+      <p>Select your favorite fruit:</p>
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="apple" 
+          checked={selectedFruit === 'apple'} 
+          onChange={handleChange} 
+        /> Apple
+      </label>
+      <br />
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="banana" 
+          checked={selectedFruit === 'banana'} 
+          onChange={handleChange} 
+        /> Banana
+      </label>
+      <br />
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="cherry" 
+          checked={selectedFruit === 'cherry'} 
+          onChange={handleChange} 
+        /> Cherry
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
 createRoot(document.getElementById('root')).render(
-  <Football />
+  <MyForm />
 );
