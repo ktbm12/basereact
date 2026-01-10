38871c2ea0c370cad4c647dd24629926324b4abf
ktbm <boliventtalla12@gmail.com>
2026-01-10 22:27:02 +0100

    upate

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import Car from './Vehicle.jsx';
-function Cars(props) {
+function Football() {
+  const shoot = () => {
+    alert("Great Shot!");
+  }
+
   return (
-    <>
-      <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
-      <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
-    </>
+    <button onClick={shoot}>Take the shot!</button>
   );
 }
 
-const carInfo = {
-  name: "Ford",
-  model: "Mustang",
-  color: "red",
-  year: 1969
-};
-
 createRoot(document.getElementById('root')).render(
-  <Cars carinfo={carInfo} />
+  <Football />
 );
-
-
-                    
