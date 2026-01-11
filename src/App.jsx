d2a7acda830aa87b75dab5d7ced3add722ef2c9d
ktbm <boliventtalla12@gmail.com>
2026-01-11 06:18:18 +0100

    update hhero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="bg-gradient-to-br from-black via-slate-900 to-indigo-900 text-white">
+      <Navbar />
+      <Hero />
     </div>
   );
 }
-
-export default App;
