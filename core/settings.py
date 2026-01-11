765b0bda709cdd9e175f2324cad658d7d524afe7
ktbm <boliventtalla12@gmail.com>
2026-01-11 12:20:39 +0100

    update models

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # LIBS
+    'rest_framework',
+    'corsheaders',
+    'django_extensions',
+
+    # CORE
+    'common',
+    'contact',
+    'cms',
 ]
 
 MIDDLEWARE = [
