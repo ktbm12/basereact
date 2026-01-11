2fbea476de2445678a07f6cfb00925267800c0c8
ktbm <boliventtalla12@gmail.com>
2026-01-11 06:30:39 +0100

    update hhero section

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // TailwindCSS ou styles globaux
 
-
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function FavoriteColor() {
-  const [color, setColor] = useState("red");
-
-  return (
-    <>
-      <h1>My favorite color is {color}!</h1>
-      <button
-        type="button"
-        onClick={() => setColor("blue")}
-      >Blue</button>
-      <button
-        type="button"
-        onClick={() => setColor("red")}
-      >Red</button>
-      <button
-        type="button"
-        onClick={() => setColor("pink")}
-      >Pink</button>
-      <button
-        type="button"
-        onClick={() => setColor("green")}
-      >Green</button>
-    </>
-  );
-}
-
-createRoot(document.getElementById('root')).render(
-  <FavoriteColor />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
