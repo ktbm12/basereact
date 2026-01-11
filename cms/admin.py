0f252412a30f96f96d6e921eac0cbbb5416cacaa
ktbm <boliventtalla12@gmail.com>
2026-01-11 12:36:14 +0100

    backend

--- cms/admin.py
+++ cms/admin.py
@@ -5,3 +5,10 @@ from .models import Project, Testimonial
 
 admin.site.register(Project)
 admin.site.register(Testimonial)
+
+
+from .models import Skill, Service, BlogPost
+
+admin.site.register(Skill)
+admin.site.register(Service)
+admin.site.register(BlogPost)
